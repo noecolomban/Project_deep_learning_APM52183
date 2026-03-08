5967a29feb7f0ddedfcb82aec98491cf2d65278b
Noé Colomban <noe.colomban@gmail.com>
2026-03-08 22:12:32 +0100

    notebook final ?

--- models/MGNN_net.py
+++ models/MGNN_net.py
@@ -2,25 +2,7 @@ import torch
 import torch.nn as nn
 import dgl.nn.pytorch as dglnn
 
-class MGNNLayer(nn.Module):
-    def __init__(self, in_features, out_features):
-        super(MGNNLayer, self).__init__()
-        self.W1 = nn.Linear(in_features, out_features, bias=False)
-        self.W2 = nn.Linear(in_features, out_features, bias=True)
-        self.activation = nn.ReLU()
 
-    def forward(self, H, A):
-        # H : tenseur de dimension (batch_size, n, in_features)
-        # A : matrice d'adjacence (batch_size, n, n)
-        
-        # Agrégation du voisinage : A * H * W1
-        neighbors_aggr = torch.bmm(A, self.W1(H)) 
-        
-        # Transformation du noeud lui-même : H * W2
-        self_feat = self.W2(H)
-        
-        return self.activation(neighbors_aggr + self_feat)
-    
 class MGNN(nn.Module):
     def __init__(self, **kwargs):
         super(MGNN, self).__init__()
