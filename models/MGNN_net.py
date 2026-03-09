4ce61723c0ac8ca4c7b31cdebb5713f48a480033
Noé Colomban <noe.colomban@gmail.com>
2026-03-09 23:12:48 +0100

    modification MGNN_net

--- models/MGNN_net.py
+++ models/MGNN_net.py
@@ -5,8 +5,8 @@ import dgl.nn.pytorch as dglnn
 
 class MGNNLayer(nn.Module):
     """
-    Couche unitaire de Message Passing pour le MGNN.
-    Applique la formule : H_new = A @ (H @ W_msg) + H @ W_self
+    Une Layer pour le MGNN
+    H_new = A @ (H @ W_msg) + H @ W_self
     """
     def __init__(self, in_features, out_features):
         super().__init__()
@@ -23,10 +23,7 @@ class MGNNLayer(nn.Module):
 
 class MGNN(nn.Module):
     def __init__(self, original_features_num, num_blocks, in_features, out_features, depth_of_mlp, input_embed=False, **kwargs):
-        """
-        Prend un batch de graphes (bs, n_vertices, n_vertices, original_features_num)
-        et renvoie un batch d'embeddings de noeuds (bs, n_vertices, out_features)
-        """
+        
         super().__init__()
         
         # On reprend les mêmes attributs que BaseModel
@@ -34,7 +31,7 @@ class MGNN(nn.Module):
         self.num_blocks = num_blocks
         self.in_features = in_features
         self.out_features = out_features
-        self.depth_of_mlp = depth_of_mlp # Gardé pour la signature, même si non utilisé ici
+        self.depth_of_mlp = depth_of_mlp #non utilisé ici
         self.embed = input_embed
         
         # Logique d'embedding copiée de BaseModel
@@ -44,37 +41,28 @@ class MGNN(nn.Module):
         else:
             first_layer_features = self.original_features_num
             
-        # Couche pour initialiser les noeuds à partir des arêtes
+        #Initialiser les noeuds à partir des arêtes
         self.edge_to_node = nn.Linear(first_layer_features, self.in_features)
         
-        # Création des blocs de Message Passing
+        # Blocs de message passing
         self.layers = nn.ModuleList()
         for i in range(self.num_blocks):
             layer_out_feat = self.out_features if i == self.num_blocks - 1 else self.in_features
             self.layers.append(MGNNLayer(self.in_features, layer_out_feat))
 
-    def forward(self, x):
-        # Validation de la taille d'entrée (comme dans BaseModel)
-        if x.size(3) != self.original_features_num:
-            print("expected input feature {} and got {}".format(self.original_features_num, x.shape[3]))
-            return
-            
-        # 1. Extraction de la matrice d'adjacence (A)
-        # On le fait avant l'embedding éventuel pour garder le sens de la matrice
+    def forward(self, x):          
+        #Matrice d'adjacence
         A = x.mean(dim=-1) 
         
-        # 2. Gestion de l'input_embed (comme dans BaseModel)
+        #Comme dans BaseModel
         if self.embed:
-            # On passe la deuxième feature (index 1) dans l'Embedding
             x_feat = self.embedding(x[:,:,:,1].long())
         else:
             x_feat = x
             
-        # 3. Création des features initiales des noeuds (H)
         h = x_feat.sum(dim=2) 
         h = self.edge_to_node(h) 
         
-        # 4. Boucle de Message Passing
         for i, layer in enumerate(self.layers):
             h = layer(h, A)
             
